39fa89862bb84b939b40f3f9ee87fa61f83531e1
Sohail Hawaldar <sohailhawaldar24@gmail.com>
2025-12-12 18:13:17 +0530

    intial commit_12122025

---
+++ routes/todoListRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { addToDo, getTodaysToDoList, deleteToDo, getTodaysToDoListByEmpId } = require('../controllers/todoListController');
+const authenticate = require("../middlewares/authMiddleware");
+
+const {
+    ADMIN,
+    CLIENT_VENDOR,
+    CLIENT,
+    PROJECT_COORDINATOR,
+} = require("../utils/constants");
+
+const router = express.Router();
+
+router.post(
+    "/addTodo",
+    authenticate([ADMIN, PROJECT_COORDINATOR]),
+    addToDo
+);
+
+router.get(
+    "/getTodoList",
+    authenticate([ADMIN, PROJECT_COORDINATOR]),
+    getTodaysToDoList
+
+);
+
+router.delete(
+    "/deleteTodo/:id",
+    authenticate([ADMIN, PROJECT_COORDINATOR]),
+    deleteToDo
+);
+
+router.get("/getTodoList/:employeeId", authenticate([ADMIN]), getTodaysToDoListByEmpId);
+module.exports = router;
